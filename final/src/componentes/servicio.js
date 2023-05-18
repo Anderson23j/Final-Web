a0983a6d66e85c32d80e97cc38b6bcc7dcbcc203
Ofaehus1 <ofaehus1@gmail.com>
2023-05-17 19:15:35 -0500

    actualizacion servicio

--- final/src/componentes/servicio.js
+++ final/src/componentes/servicio.js
@@ -1,15 +1,30 @@
-import React from "react";
-/* import "../styles/error.css" */
+import React from 'react';
+import '../styles/servicio.css';
 
-function Servicios() {
+const cardsData = [
+  { title: 'MECANICA BASICA', content: 'Alineación y Balanceo Cambio de Aceite Cambio de Frenos Sincronización' },
+  { title: 'MECANICA ESPECIALIZADA', content: 'Aire acondicionado Amortiguadores Correa de Repartición Eléctrica y Electrónica Kit de Embrague Reparación de Motor Suspensión' },
+  { title: 'REVISION AUTOMOTRIZ', content: 'Diagnóstico Automotriz Peritaje Revisión Mecánica Revisión Por Kilometraje Revisión Tecno-Mecánica' },
+  { title: 'ESTETICA AUTOMOTRIZ', content: 'Latoneria Pintura' },
+];
+
+const Card = ({ title, content }) => {
   return (
-    <>
-      <div className="">
-        <h1>Estas en servicios</h1>
-        <img clas src={(require = "")} />
-        <p className=""> lorem </p>
-      </div>
-    </>
+    <div className="card">
+      <h3>{title}</h3>
+      <p>{content}</p>
+    </div>
   );
-}
-export default Servicios;
+};
+
+const servicio = () => {
+  return (
+    <div className="container">
+      {cardsData.map((card, index) => (
+        <Card key={index} title={card.title} content={card.content} />
+      ))}
+    </div>
+  );
+};
+
+export default servicio;
