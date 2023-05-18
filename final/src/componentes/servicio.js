c9d5d52e190a24bc6de8ba3efe2b78be89d5bef2
Ofaehus1 <ofaehus1@gmail.com>
2023-05-17 19:35:04 -0500

    actualizacion

--- final/src/componentes/servicio.js
+++ final/src/componentes/servicio.js
@@ -2,29 +2,31 @@ import React from 'react';
 import '../styles/servicio.css';
 
 const cardsData = [
-  { title: 'MECANICA BASICA', content: 'Alineación y Balanceo Cambio de Aceite Cambio de Frenos Sincronización' },
-  { title: 'MECANICA ESPECIALIZADA', content: 'Aire acondicionado Amortiguadores Correa de Repartición Eléctrica y Electrónica Kit de Embrague Reparación de Motor Suspensión' },
-  { title: 'REVISION AUTOMOTRIZ', content: 'Diagnóstico Automotriz Peritaje Revisión Mecánica Revisión Por Kilometraje Revisión Tecno-Mecánica' },
-  { title: 'ESTETICA AUTOMOTRIZ', content: 'Latoneria Pintura' },
+  { title: 'MECANICA BASICA', content: 'Alineación y Balanceo<br>Cambio de Aceite<br>Cambio de Frenos<br>Sincronización' },
+  { title: 'MECANICA ESPECIALIZADA', content: 'Aire acondicionado<br>Amortiguadores<br>Correa de Repartición<br>Eléctrica y Electrónica<br>Kit de Embrague<br>Reparación de Motor<br>Suspensión' },
+  { title: 'REVISION AUTOMOTRIZ', content: 'Diagnóstico Automotriz<br>Peritaje<br>Revisión Mecánica<br>Revisión Por Kilometraje<br>Revisión Tecno-Mecánica' },
+  { title: 'ESTETICA AUTOMOTRIZ', content: 'Latoneria<br>Pintura' },
 ];
 
 const Card = ({ title, content }) => {
   return (
     <div className="card">
       <h3>{title}</h3>
-      <p>{content}</p>
+      <p dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
 };
 
-const servicio = () => {
+const Servicio = () => {
   return (
     <div className="container">
-      {cardsData.map((card, index) => (
-        <Card key={index} title={card.title} content={card.content} />
-      ))}
+      <div className="grid">
+        {cardsData.map((card, index) => (
+          <Card key={index} title={card.title} content={card.content} />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default servicio;
+export default Servicio;
