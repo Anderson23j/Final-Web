4ccaea9a0c053939217d216cbc0ff78e12b631bd
Mateo <ofaehus1@gmail.com>
2023-05-20 11:28:47 -0500

    ac

--- final/src/componentes/nosotros.js
+++ final/src/componentes/nosotros.js
@@ -5,13 +5,13 @@ const nosotros = () => {
   return (
     <div className="container">
       <div className="card">
-        <h2 className="card-title">Carta 1</h2>
-        <p className="card-text">Texto de la carta 1.</p>
+        <h2 className="card-title">Mision</h2>
+        <p className="card-text">Ser una empresa especializad repuestos, accesorios, mantenimiento preventivo y correctivo y trabajos de carrocería y pintura. Contar con personal altamente cualificado, tecnología punta e infraestructura que garanticen siempre un servicio integral de máxima calidad. Cumplir con los estándares de las marca, normas ambientales y del entorno social y trabajar por el bienestar y crecimiento de nuestro talento humano.</p>
       </div>
 
       <div className="card">
-        <h2 className="card-title">Carta 2</h2>
-        <p className="card-text">Texto de la carta 2.</p>
+        <h2 className="card-title">Vision</h2>
+        <p className="card-text">Ser la empresa referente y mantener la posición de liderazgo en su mercado, manteniendo unos volúmenes que le proporcionen una rentabilidad adecuada para la continuidad de la misma. Ser una empresa colombiana de referencia en el sector automotriz en calidad, tecnología, infraestructura, capital humano, rentabilidad y solidez financiera, excediendo las expectativas de clientes, empleados y proveedores.</p>
       </div>
     </div>
   );
