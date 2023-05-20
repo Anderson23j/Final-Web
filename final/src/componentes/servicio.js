77a5437a604d7715fefb77733baf6922e71372fb
Mateo <ofaehus1@gmail.com>
2023-05-20 10:35:20 -0500

    actualizacion

--- final/src/componentes/servicio.js
+++ final/src/componentes/servicio.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import '../styles/servicio.css';
 
-const cardsData = [
-  { title: 'MECANICA BASICA', content: 'Alineación y Balanceo<br>Cambio de Aceite<br>Cambio de Frenos<br>Sincronización' },
-  { title: 'MECANICA ESPECIALIZADA', content: 'Aire acondicionado<br>Amortiguadores<br>Correa de Repartición<br>Eléctrica y Electrónica<br>Kit de Embrague<br>Reparación de Motor<br>Suspensión' },
-  { title: 'REVISION AUTOMOTRIZ', content: 'Diagnóstico Automotriz<br>Peritaje<br>Revisión Mecánica<br>Revisión Por Kilometraje<br>Revisión Tecno-Mecánica' },
-  { title: 'ESTETICA AUTOMOTRIZ', content: 'Latoneria<br>Pintura' },
-];
-
-const Card = ({ title, content }) => {
+const CartasAzules = () => {
   return (
-    <div className="card">
-      <h3>{title}</h3>
-      <p dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
-  );
-};
-
-const Servicio = () => {
-  return (
-    <div className="container">
-      <div className="grid">
-        {cardsData.map((card, index) => (
-          <Card key={index} title={card.title} content={card.content} />
-        ))}
+    <div className="contenedor">
+      <div className="carta">
+        <div className="contenido">
+          <h3>MECANICA BASICA</h3>
+          <p contentEditable="true" className="editable">
+            Texto de la Carta 1
+          </p>
+        </div>
+      </div>
+      <div className="carta">
+        <div className="contenido">
+          <h3>MECANICA ESPECIALIZADA</h3>
+          <p contentEditable="true" className="editable">
+            asds
+          </p>
+        </div>
+      </div>
+      <div className="carta">
+        <div className="contenido">
+          <h3>ESTETICA AUTOMOTRIZ</h3>
+          <p contentEditable="true" className="editable">
+            ASDASDA
+          </p>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Servicio;
+export default CartasAzules;
